517366d5b86d7d6317526df8ef7d3b64ae94c2d8
Tony Narisi <email": null>
2016-07-23 12:49:21 -0400

    Create Apartment class

--- building.rb
+++ building.rb
@@ -1,2 +1,29 @@
 class Building
+
+  attr_reader :apartments
+
+  def initialize(args={})
+    @address = args.fetch(:address, "")
+    @apartments = args.fetch(:apartments, [])
+  end
+
+  def total_room_count
+    @apartments.inject(0) { |sum, apt| sum += apt.rooms.length }
+  end
+
+  def total_monthly_revenue
+    @apartments.inject(0) { |sum, apt| sum += apt.monthly_rent }
+  end
+
+  def apartments_by_rent
+    @apartments.sort_by { |apt| apt.monthly_rent }.reverse
+  end
+
+  def find_apartments_by_bedroom_count(num)
+    @apartments.find { |apt| apt.bedroom_count == num }
+  end
+
+  def total_sqft
+    @apartments.inject(0) { |sum, apt| sum += apt.total_sqft }
+  end
 end

9a878f3e88844f1aa7569f899e45f873eb8e4eb2
mattk5 <mattkubota5@gmail.com>
2016-07-24 09:22:28 -0400

    completed release 1 to make tests pass.

--- building.rb
+++ building.rb
@@ -1,2 +1,9 @@
-class Building
+class Building 
+	attr_reader :apartments
+	def initialize(args = {})
+		@apartments = args.fetch(:apartments, "")
+		@address = args.fetch(:address, "48 Wall St.")
+	end
+
+
 end

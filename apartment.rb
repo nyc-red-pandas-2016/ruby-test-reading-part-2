511f494e19eaa54e7a97360059847fc20cd2cfad
Gabriel Mahan <mahan.gabe@gmail.com>
2016-07-24 17:46:01 -0400

    All tests pass

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,29 @@
+require_relative 'room'
+
 class Apartment
+
+	attr_accessor :rooms, :number, :monthly_rent
+
+	def initialize(args = {})
+		@rooms = args.fetch(:rooms, [])
+		@number = args.fetch(:number, 'unlisted')
+		@monthly_rent = args.fetch(:monthly_rent, 1000)
+	end
+
+	def total_sqft
+		rooms.map { |room| room.sqft }.inject(:+)
+	end
+
+	def price_per_sqft
+		monthly_rent.to_f / total_sqft.to_f
+	end
+
+	def room_count
+		rooms.length
+	end
+
+	def bedroom_count
+		rooms.select { |room| room.name.downcase.strip == 'bedroom' }.length
+	end
+	
 end
--- room.rb
+++ room.rb
@@ -1,2 +1,11 @@
 class Room
+
+	attr_accessor :name, :sqft, :window_count
+
+	def initialize(args={})
+		@name = args.fetch(:name, "bedroom")
+		@sqft = args.fetch(:sqft, 120)
+		@window_count = args.fetch(:window_count, 1)
+	end
+
 end

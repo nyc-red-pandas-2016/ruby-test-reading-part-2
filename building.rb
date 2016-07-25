fcb7a3176fc2469cbcb8c078d789159dd63b8f2f
Dave Lin & Myra Orgain <pair+dvdlin214.mlorgain@devbootcamp.com>
2016-07-25 15:37:27 -0400

    Complete room and apartment, need to complete building

--- building.rb
+++ building.rb
@@ -1,2 +1,19 @@
 class Building
+
+  attr_reader :address, :apartments
+
+  def initialize(args = {})
+    @address = args.fetch(:address, '')
+    @apartments = args.fetch(:apartments, [])
+  end
+
+  def total_room_count
+    room_array = @apartments.map{ |apt| apt.rooms }
+    room_array.each { |room| }
+  end
+
+  def total_monthly_revenue
+
+  end
+
 end
